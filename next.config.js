45277bc4cee697bf465227033235710216ac70f1
Henrique Batista <wellington.h.batista@hotmail.com>
2023-09-04 08:26:40 -0300

    Cria a configurações do arquivo

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'zegchxphjrcpwphsexkh.supabase.co',
+      'imgnike-a.akamaihd.net',
+      'static.lojanba.com',
+      'static.netshoes.com.br',
+      'artwalk.vteximg.com.br'
+    ]
+  }
+}
+
+module.exports = nextConfig
